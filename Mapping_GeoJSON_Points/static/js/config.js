a7f56d67b32dd04ab03fdfbaecf04722f5960a52
leece <you@example.com>
2022-10-20 11:36:52 -0400

    Adding toronto line routes

---
+++ Mapping_GeoJSON_Points/static/js/config.js
@@ -0,0 +1,5 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// API Key
+const API_Key = "pk.eyJ1IjoiY2FzZXlsZWUyOCIsImEiOiJjbDllc29kOHozZWRnM29xbTI2dG81dXlzIn0.Ks6tpjGqgq-YpoeFlJYM6w";
---
+++ Mapping_Lines/static/js/config.js
@@ -0,0 +1,3 @@
+
+// API Key
+const API_Key = "pk.eyJ1IjoiY2FzZXlsZWUyOCIsImEiOiJjbDllc29kOHozZWRnM29xbTI2dG81dXlzIn0.Ks6tpjGqgq-YpoeFlJYM6w";
